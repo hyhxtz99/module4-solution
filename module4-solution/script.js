cc3f6cfb9bd9534b93c6b7addfaebb25c2d37f5b
hyhxtz99 <139350685+hyhxtz99@users.noreply.github.com>
2023-08-04 16:28:36 +0800

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -27,6 +27,17 @@ The code does NOT currently work! It is YOUR job to make it work
 as described in the requirements and the steps in order to complete this
 assignment.
 WARNING!!! WARNING!!!
+var byeSpeaker={};
+// STEP 7: Create an object, called 'byeSpeaker' to which you will attach
+// the "speak" method and which you will expose to the global context
+// See Lecture 52, part 1
+
+
+
+var speakWord = "Good Bye";
+byeSpeaker.speak=function(name) {
+  console.log(speakWord + " " + name);
+}
 
 */
 
