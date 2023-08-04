fc5459d274327b9a44efb4dbc5e70d939a0b6f3a
hyhxtz99 <139350685+hyhxtz99@users.noreply.github.com>
2023-08-04 16:32:21 +0800

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -10,23 +10,8 @@
 // The program should say "Hello" to any name except names that start with a "J"
 // or "j", otherwise, the program should say "Good Bye". So, the final output
 // on the console should look like this:
-/*
-Hello Yaakov
-Good Bye John
-Good Bye Jen
-Good Bye Jason
-Hello Paul
-Hello Frank
-Hello Larry
-Hello Paula
-Hello Laura
-Good Bye Jim
 
-WARNING!!! WARNING!!!
-The code does NOT currently work! It is YOUR job to make it work
-as described in the requirements and the steps in order to complete this
-assignment.
-WARNING!!! WARNING!!!
+
 var byeSpeaker={};
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
@@ -39,7 +24,7 @@ byeSpeaker.speak=function(name) {
   console.log(speakWord + " " + name);
 }
 
-*/
+
 
 // STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Wrap the entire contents of script.js inside of an IIFE
